47358c856a32d88d71ac250f4928811a713287a5
cversteeg <chrissversteeg@gmail.com>
2023-07-10 17:11:08 -0400

    Restructure Repo

---
+++ interpretability/task_modeling/task_trained_wrapper/tasktrain_wrapper.py
@@ -0,0 +1,37 @@
+from interpretability.task_modeling.datamodule.task_datamodule import TaskDataModule
+class TaskTrainWrapper():
+    def __init__(self, task_env, model, data_simulator, trainer):
+        self.task_env = task_env
+        self.model = model
+        self.data_simulator = data_simulator
+        self.trainer = trainer
+        self.datamodule = None
+        self.seed = 0
+    
+    def create_datamodule(self):
+        self.datamodule = TaskDataModule(
+            task_env = self.task_env,
+            n_samples = self.task_env.n_samples,
+            n_timesteps = self.task_env.n_timesteps,
+            )
+        
+    def tune_model(self):
+        if self.datamodule is None:
+            self.create_datamodule()
+        self.trainer.tune(model = self.model, datamodule = self.datamodule)
+
+    def train_model(self):
+        if self.datamodule is None:
+            self.create_datamodule()
+        self.trainer.fit(model = self.model, datamodule = self.datamodule)
+
+    def simulate_neural_spiking(self):
+        # Pass the latent activity to the data simulator to generate spiking data
+        self.data_simulator.simulate_neural_data(
+            task_trained_model = self.model, 
+            datamodule = self.datamodule,
+            seed = self.seed
+            )
+    def train_and_simulate(self):
+        self.train_model()
+        self.simulate_neural_spiking()
--- task_modeling/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="task_modeling",
-    version="1.0",
-    packages=find_packages(),
-)
