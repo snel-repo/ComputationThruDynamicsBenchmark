6ba7abbefa5f769ef61dfbc16c3d07df7f6d197b
cversteeg <chrissversteeg@gmail.com>
2024-03-13 09:48:05 -0400

    Changing ctd

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 with open("requirements.txt") as file:
     requirements = file.read().splitlines()
 setup(
-    name="interpretability",
+    name="ctd",
     version="1.0",
     install_requires=requirements,
     packages=find_packages(),
